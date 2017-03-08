61bf512bcf4bf2f2955ac64a02ac1a8e0bed5da5
Philip Ye <phye@cisco.com>
2017-03-08 21:53:14 +0800

    Use bower to install bootstrap into ember

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -19,6 +20,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
   return app.toTree();
 };
